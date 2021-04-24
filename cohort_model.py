865a1fe92e334d902a1440d2d4cdc9e71bc82111
Oda Omholt <oda.omh@gmail.com>
2021-04-24 13:19:01 +0200

    Add additional functions to get clean code for figures 1, 2 and 3

--- cohort_model.py
+++ cohort_model.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 from typing import Tuple
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import brentq
 
 MUTANT_CAP = "mutant captivity"
 MUTANT_WILD = "mutant wild"
@@ -12,66 +15,14 @@ HYP_WILD_TYPE = "hypothetical wild type"
 # TODO: Add checks for incorrect parameter sets
 
 
-def darwinian_lottery(cohort: np.ndarray, hazard_rate_parameters: dict) -> np.ndarray:
-    """[summary]
-
-    Parameters
-    ----------
-    cohort : np.ndarray
-        [description]
-    hazard_rate_parameters : dict
-        [description]
-
-    Returns
-    -------
-    np.ndarray
-        [description]
-    """
-    number_of_individuals = cohort.shape[0]
-    hazard = get_hazard_rate(**hazard_rate_parameters)
-    lottery_tickets = cohort * np.random.random_sample(number_of_individuals)
-    survivors = np.piecewise(
-        lottery_tickets, [lottery_tickets <= hazard, lottery_tickets > hazard], [0, 1]
-    )
-    return survivors
-
-
-def cohort_survivorship_model(cohort_survivorship: np.ndarray, hazard_rate_parameters: dict, t_m: int) -> np.ndarray:
-    """[summary]
-
-    Parameters
-    ----------
-    cohort_survivorship : np.ndarray
-        [description]
-    hazard_rate_parameters : dict
-        [description]
-    t_m : int
-        [description]
-
-    Returns
-    -------
-    np.ndarray
-        [description]
-    """
-    # On first run, make 1D array into 2D
-    if len(cohort_survivorship.shape) == 1:
-        cohort_survivorship = cohort_survivorship.reshape(1, -1)
-
-    t = cohort_survivorship.shape[0]  # Time step variable
-    hazard_rate_parameters["time"] = t
-
-    current_cohort = cohort_survivorship[-1]
-
-    if t >= t_m:
-        return cohort_survivorship
-    survivors = darwinian_lottery(current_cohort, hazard_rate_parameters)
-    cohort_survivorship = np.append(
-        cohort_survivorship, survivors.reshape(1, -1), axis=0
-    )
-    return cohort_survivorship_model(cohort_survivorship, hazard_rate_parameters, t_m)
-
-
-def get_hazard_rate(population: str, epsilon: float, hazard_rate_wild_type: float, alpha: float, kappa: float, t: int) -> float:
+def get_hazard_rate(
+    population: str,
+    epsilon: float,
+    hazard_rate_wild_type: float,
+    alpha: float,
+    kappa: float,
+    t: int,
+) -> float:
     """[summary]
 
     Parameters
@@ -113,8 +64,73 @@ def get_hazard_rate(population: str, epsilon: float, hazard_rate_wild_type: floa
     )
 
 
+def darwinian_lottery(cohort: np.ndarray, hazard_rate_parameters: dict) -> np.ndarray:
+    """[summary]
+
+    Parameters
+    ----------
+    cohort : np.ndarray
+        [description]
+    hazard_rate_parameters : dict
+        [description]
+
+    Returns
+    -------
+    np.ndarray
+        [description]
+    """
+    number_of_individuals = cohort.shape[0]
+    hazard = get_hazard_rate(**hazard_rate_parameters)
+    lottery_tickets = cohort * np.random.random_sample(number_of_individuals)
+    survivors = np.piecewise(
+        lottery_tickets, [lottery_tickets <= hazard, lottery_tickets > hazard], [0, 1]
+    )
+    return survivors
+
+
+def cohort_survivorship_model(
+    cohort_survivorship: np.ndarray, hazard_rate_parameters: dict, t_m: int
+) -> np.ndarray:
+    """[summary]
+
+    Parameters
+    ----------
+    cohort_survivorship : np.ndarray
+        [description]
+    hazard_rate_parameters : dict
+        [description]
+    t_m : int
+        [description]
+
+    Returns
+    -------
+    np.ndarray
+        [description]
+    """
+    # On first run, make 1D array into 2D
+    if len(cohort_survivorship.shape) == 1:
+        cohort_survivorship = cohort_survivorship.reshape(1, -1)
+
+    t = cohort_survivorship.shape[0]  # Time step variable
+    hazard_rate_parameters["t"] = t
+
+    current_cohort = cohort_survivorship[-1]
+
+    if t >= t_m:
+        return cohort_survivorship
+    survivors = darwinian_lottery(current_cohort, hazard_rate_parameters)
+    cohort_survivorship = np.append(
+        cohort_survivorship, survivors.reshape(1, -1), axis=0
+    )
+    return cohort_survivorship_model(cohort_survivorship, hazard_rate_parameters, t_m)
+
+
 def run_simulation(
-    number_of_repetitions: int, cohort_survivorship: np.ndarray, hazard_rate_parameters: dict, t_m: int, population: str
+    number_of_repetitions: int,
+    cohort: np.ndarray,
+    hazard_rate_parameters: dict,
+    t_m: int,
+    population: str,
 ) -> np.ndarray:
     """[summary]
 
@@ -122,7 +138,7 @@ def run_simulation(
     ----------
     number_of_repetitions : int
         [description]
-    cohort_survivorship : np.ndarray
+    cohort : np.ndarray
         [description]
     hazard_rate_parameters : dict
         [description]
@@ -141,13 +157,63 @@ def run_simulation(
     population_survivorship = []
     for _ in range(number_of_repetitions):
         cohort_survivorship = np.sum(
-            cohort_survivorship_model(cohort_survivorship, hazard_rate_parameters, t_m),
+            cohort_survivorship_model(cohort, hazard_rate_parameters, t_m),
             axis=1,
             dtype=int,
         )
         population_survivorship.append(cohort_survivorship)
 
     return np.array(population_survivorship)
+
+
+def get_cohort_model_data(
+    t_m_captivity: int,
+    t_m_wild: int,
+    t_m_hyp_wt: int, 
+    number_of_individuals: int,
+    alpha: float,
+    kappa: float,
+    epsilon: float,
+    hazard_rate_wild_type: float,
+    number_of_repetitions: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """[summary]
+
+    Parameters
+    ----------
+    t_m_captivity : int
+        [description]
+    t_m_wild : int
+        [description]
+    t_m_hyp_wt : int
+        [description]
+    number_of_individuals : int
+        [description]
+    alpha : float
+        [description]
+    kappa : float
+        [description]
+    epsilon : float
+        [description]
+    hazard_rate_wild_type : float
+        [description]
+    number_of_repetitions : int
+        [description]
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray, np.ndarray]
+        [description]
+    """
+
+    cohort = np.ones(number_of_individuals)
+
+    hazard_rate_parameters = dict(epsilon=epsilon, hazard_rate_wild_type=hazard_rate_wild_type, alpha=alpha, kappa=kappa)
+
+    captivity_population = run_simulation(number_of_repetitions, cohort, hazard_rate_parameters, t_m_captivity, MUTANT_CAP)
+    wild_population = run_simulation(number_of_repetitions, cohort, hazard_rate_parameters, t_m_wild, MUTANT_WILD)
+    hyp_wt_population = run_simulation(number_of_repetitions, cohort, hazard_rate_parameters, t_m_hyp_wt, HYP_WILD_TYPE)
+
+    return captivity_population, wild_population, hyp_wt_population
 
 
 def get_mean_and_std(population_survivorship: np.ndarray) -> Tuple[float, float]:
@@ -169,90 +235,24 @@ def get_mean_and_std(population_survivorship: np.ndarray) -> Tuple[float, float]
     return mean, std
 
 
-def plot_fig_1(
-    t_m_captivity: int,
-    t_m_wild: int,
-    t_m_hyp_wt:int,
-    captivity_population: np.ndarray,
-    wild_population: np.ndarray,
-    hyp_wt_population: np.ndarray,
-):
-    """[summary]
+def population_survivorship_difference(number_of_individuals, number_of_repetitions, epsilons, hazard_rates_wt, alpha, kappa, t_m, populations=(MUTANT_WILD, HYP_WILD_TYPE)):
+    # #Calculates the difference in number of survivors between hypothetical wild type and mutant across the given time span 
+    cohort = np.ones(number_of_individuals)
 
-    Parameters
-    ----------
-    t_m_captivity : int
-        [description]
-    t_m_wild : int
-        [description]
-    t_m_hyp_wt : int
-        [description]
-    captivity_population : np.ndarray
-        [description]
-    wild_population : np.ndarray
-        [description]
-    hyp_wt_population : np.ndarray
-        [description]
-    """
-    # TODO: Én eller to variabler for de to t_m som skal være like?
+    population_simulations = defaultdict(list)
 
-    fig, ax = plt.subplots(figsize=(6, 6))
+    for population in populations:
+        np.random.seed(1729) # Reset seed to produce the same pseudo-random number sequence for each population
+        for epsilon, hazard_rate_wt in zip(epsilons, hazard_rates_wt):
+            hazard_rate_parameters = dict(epsilon=epsilon, hazard_rate_wild_type=hazard_rate_wt, alpha=alpha, kappa=kappa)
+            cohort_simulation = run_simulation(number_of_repetitions, cohort, hazard_rate_parameters, t_m, population)
+            population_simulations[population].append(cohort_simulation)
+    
+    # TODO: litt dårlig kode her med både populations og MUTANT_WILD, HYP_WILD_TYPE konstantene 
+    diff = np.array(population_simulations[MUTANT_WILD]) - np.array(population_simulations[HYP_WILD_TYPE])
+    mean_diff = np.mean(diff, axis=1)
+    std_diff = np.std(diff, axis=1)
 
-    captivity_x = np.arange(0, t_m_captivity, 1, dtype=int)
-    wild_x = np.arange(0, t_m_wild, 1, dtype=int)
-
-    mean_captivity, std_captivity = get_mean_and_std(captivity_population)
-    mean_wild, _ = get_mean_and_std(wild_population)
-    mean_hyp_wt, std_hyp_wt = get_mean_and_std(hyp_wt_population)
-
-    ax.plot(captivity_x, mean_captivity, "r-")
-    ax.fill_between(
-        range(t_m_captivity),
-        mean_captivity - 3.0 * std_captivity,
-        mean_captivity + 3.0 * std_captivity,
-        color="pink",
-        alpha=0.5,
-    )
-
-    ax.plot(wild_x, mean_wild, "b-")
-    ax.fill_between(
-        range(t_m_hyp_wt),
-        mean_hyp_wt - 3.0 * std_hyp_wt,
-        mean_hyp_wt + 3.0 * std_hyp_wt,
-        color="lightblue",
-        alpha=0.5,
-    )
-
-    # Empirical data from Austad (1989)
-    # TODO: Import from text file, show calculations etc.
-    X_C = [0, 30 * 217 / 65, 40 * 217 / 65, 51 * 217 / 65, 60 * 217 / 65]
-    Y_C = [1.0, 0.8895, 0.592, 0.174, 0.0239]
-    ax.plot(X_C, Y_C, "ro", markersize=4)
-
-    X_W = [0, 5 * 100 / 30, 10 * 100 / 30, 15 * 100 / 30, 20 * 100 / 30, 30 * 100 / 30]
-    Y_W = [1.0, 0.5498, 0.268, 0.148, 0.05, 0.0]
-    ax.plot(X_W, Y_W, "bo", markersize=4)
-
-    afont = {"fontname": "Arial"}
-    ax.set_xlabel("Time", fontsize=12, **afont)
-    ax.set_ylabel("Cohort survivorship", fontsize=12, **afont)
-
-    x_size = 10
-    plt.rc("xtick", labelsize=x_size)
-    y_size = 10
-    plt.rc("ytick", labelsize=y_size)
-
-    fig.tight_layout()
-
-    figure = plt.gcf()
-    figure.set_size_inches(3.42, 3.42)
-
-    plt.savefig("PNAS_fig1_Frontinella.pdf", dpi=1200, bbox_inches="tight")
+    return population_simulations, mean_diff, std_diff
 
 
-def plot_fig_2():
-    pass
-
-
-if __name__ == "__main__":
-    pass
